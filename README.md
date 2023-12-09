de8ce71ef6a216954957fc4d07979b58f99768fa
zingzongzung <the.zingzongzung@gmail.com>
2023-12-10 09:14:19 +1100

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-hello
+# Soroban Rust Smart Contract
+
+This Rust smart contract is designed to provide messaging, donations, and contact management functionalities on the Soroban blockchain. The contract is implemented using the Soroban SDK.
+
+## Prerequisites
+
+- **Rust Programming Language**: Ensure that you have Rust installed on your development machine.
+- **Soroban SDK**: The smart contract relies on the Soroban SDK for blockchain interactions. Make sure to include the `soroban_sdk` crate in your project dependencies.
+
+## Overview
+
+The smart contract defines a `Contract` struct and implements various methods to initialize the contract, update asset addresses, send donations, send messages, retrieve messages, and manage contacts.
+
+### Contract Initialization
+
+#### `initialize`
+
+This method initializes the smart contract with the provided administrator and asset addresses. It checks if the contract has already been initialized and panics if it has.
+
+### Update Asset Address
+
+#### `update_asset_address`
+
+This method allows the administrator to update the asset address. It verifies that the contract is initialized and that the caller is the administrator.
+
+### Sending Donations and Messages
+
+#### `send_donation`
+
+This method facilitates the transfer of tokens (`donation_amount`) from one address (`from`) to another address (`to`). It also sends a donation message.
+
+#### `send_message`
+
+This method sends a text message from one address (`from`) to another address (`to`).
+
+### Retrieving Messages and Contacts
+
+#### `get_messages`
+
+This method retrieves messages between the specified `origin` and `target` addresses.
+
+#### `get_contacts`
+
+This method retrieves the contact list for a given user.
+
+## Internal Functions
+
+The smart contract also defines several internal functions used to manage messages and contacts. These functions include:
+
+- `get_storage_messages`: Retrieves stored messages between two addresses.
+- `get_storage_contacts`: Retrieves the contact list for a given user.
+- `add_contact`: Adds a contact to the user's contact list.
+- `add_message`: Adds a message to the storage.
+
+## Usage Example
+
+Here's a brief example of how to use the smart contract:
+
+```rust
+// Initialize the contract
+Contract::initialize(env, admin_address, asset_address);
+
+// Update asset address
+Contract::update_asset_address(env, admin_address, new_asset_address);
+
+// Send a donation
+Contract::send_donation(env, from_address, to_address, donation_amount);
+
+// Send a text message
+Contract::send_message(env, from_address, to_address, text_message);
+
+// Retrieve messages
+let messages = Contract::get_messages(env, user1_address, user2_address);
+
+// Retrieve contacts
+let contacts = Contract::get_contacts(env, user_address);
+```
--- test.sh
+++ test.sh
@@ -1,32 +1,36 @@
 # soroban config identity generate admin
 # zingzongzung@zingzonzungsMBP message % soroban config identity fund admin --network testnet
-ASSET_ADDRESS=$(soroban lab token id --asset native --network testnet)
+ASSET_ADDRESS=$(soroban lab token id --asset native --network futurenet)
 echo "Stellar address " $ASSET_ADDRESS
 soroban contract build
-CONTRACT_ID=$(soroban contract deploy --wasm target/wasm32-unknown-unknown/release/skirmish11.wasm --source rpciege --network testnet )
+CONTRACT_ID=$(soroban contract deploy --wasm target/wasm32-unknown-unknown/release/soroban_chat.wasm --source rpciege --network futurenet )
 echo $CONTRACT_ID
+
+soroban contract invoke --id CBGFLK7ENPKPKNW7UQVWZFZHCVZVCY37VXVARVEFZ52V55BPFJ3ZHU7D --source rpciege --network futurenet -- send_message --from $(soroban config identity address rpciege) --to GCBELBPTCGSGFT3D2Q53CAQTEA25RDTNOX4I6OCNVMLGYZ4B4TM3VZZH  --message 48656c6c6f207468657265
+	
  
  
 teste_messages(){ 
 	echo "Running all commands" 
 
-	soroban contract invoke --id $CONTRACT_ID --source admin --network testnet -- initialize --admin $(soroban config identity address admin)  --asset_address $ASSET_ADDRESS
+	soroban contract invoke --id $CONTRACT_ID --source admin --network futurenet -- initialize --admin $(soroban config identity address admin)  --asset_address $ASSET_ADDRESS
 	
 	
-	soroban contract invoke --id $CONTRACT_ID --source rpciege --network testnet -- send_message --from $(soroban config identity address rpciege) --to $(soroban config identity address user_a)  --message 48656c6c6f207468657265
-	soroban contract invoke --id $CONTRACT_ID --source user_a --network testnet -- send_message --from $(soroban config identity address user_a) --to $(soroban config identity address rpciege)  --message 48656c6c6f207468657265
-	soroban contract invoke --id $CONTRACT_ID --source user_a --network testnet -- send_donation --from $(soroban config identity address user_a) --to $(soroban config identity address rpciege)  --donation_amount 123
+	soroban contract invoke --id $CONTRACT_ID --source rpciege --network futurenet -- send_message --from $(soroban config identity address rpciege) --to $(soroban config identity address user_a)  --message 48656c6c6f207468657265
+	soroban contract invoke --id $CONTRACT_ID --source user_a --network futurenet -- send_message --from $(soroban config identity address user_a) --to $(soroban config identity address rpciege)  --message 48656c6c6f207468657265
+	soroban contract invoke --id $CONTRACT_ID --source user_a --network futurenet -- send_donation --from $(soroban config identity address user_a) --to $(soroban config identity address rpciege)  --donation_amount 123
 
-	soroban contract invoke --id $CONTRACT_ID --source rpciege --network testnet -- get_messages --origin $(soroban config identity address rpciege) --target $(soroban config identity address user_a)   
-	soroban contract invoke --id $CONTRACT_ID --source rpciege --network testnet -- get_contacts --user_address $(soroban config identity address user_a) 
+	soroban contract invoke --id $CONTRACT_ID --source rpciege --network futurenet -- get_messages --origin $(soroban config identity address rpciege) --target $(soroban config identity address user_a)   
+	soroban contract invoke --id $CONTRACT_ID --source rpciege --network futurenet -- get_contacts --user_address $(soroban config identity address user_a) 
 	
 }
 
 init(){ 
 	echo "Init" 
 
-	soroban contract invoke --id $CONTRACT_ID --source admin --network testnet -- initialize --admin $(soroban config identity address admin)  --asset_address $ASSET_ADDRESS	
+	soroban contract invoke --id $CONTRACT_ID --source admin --network futurenet -- initialize --admin $(soroban config identity address admin)  --asset_address $ASSET_ADDRESS	
 }
 
+	soroban contract invoke --id CBGFLK7ENPKPKNW7UQVWZFZHCVZVCY37VXVARVEFZ52V55BPFJ3ZHU7D --source rpciege --network futurenet -- get_messages --origin $(soroban config identity address rpciege) --target GCBELBPTCGSGFT3D2Q53CAQTEA25RDTNOX4I6OCNVMLGYZ4B4TM3VZZH
 
  init
