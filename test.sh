2d4624c8036de0e8b80f06bdfd1d2ac8f7b45eae
zingzongzung <the.zingzongzung@gmail.com>
2023-12-10 09:25:40 +1100

    Script to run tests

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@
 ASSET_ADDRESS=$(soroban lab token id --asset native --network futurenet)
 echo "Stellar address " $ASSET_ADDRESS
 soroban contract build
-CONTRACT_ID=$(soroban contract deploy --wasm target/wasm32-unknown-unknown/release/soroban_chat.wasm --source rpciege --network futurenet )
+CONTRACT_ID=$(soroban contract deploy --wasm target/wasm32-unknown-unknown/release/soroban_chat.wasm --source user_b --network futurenet )
 echo $CONTRACT_ID
 
-soroban contract invoke --id CBGFLK7ENPKPKNW7UQVWZFZHCVZVCY37VXVARVEFZ52V55BPFJ3ZHU7D --source rpciege --network futurenet -- send_message --from $(soroban config identity address rpciege) --to GCBELBPTCGSGFT3D2Q53CAQTEA25RDTNOX4I6OCNVMLGYZ4B4TM3VZZH  --message 48656c6c6f207468657265
+soroban contract invoke --id CBGFLK7ENPKPKNW7UQVWZFZHCVZVCY37VXVARVEFZ52V55BPFJ3ZHU7D --source user_b --network futurenet -- send_message --from $(soroban config identity address user_b) --to GCBELBPTCGSGFT3D2Q53CAQTEA25RDTNOX4I6OCNVMLGYZ4B4TM3VZZH  --message 48656c6c6f207468657265
 	
  
  
@@ -16,12 +16,12 @@ teste_messages(){
 	soroban contract invoke --id $CONTRACT_ID --source admin --network futurenet -- initialize --admin $(soroban config identity address admin)  --asset_address $ASSET_ADDRESS
 	
 	
-	soroban contract invoke --id $CONTRACT_ID --source rpciege --network futurenet -- send_message --from $(soroban config identity address rpciege) --to $(soroban config identity address user_a)  --message 48656c6c6f207468657265
-	soroban contract invoke --id $CONTRACT_ID --source user_a --network futurenet -- send_message --from $(soroban config identity address user_a) --to $(soroban config identity address rpciege)  --message 48656c6c6f207468657265
-	soroban contract invoke --id $CONTRACT_ID --source user_a --network futurenet -- send_donation --from $(soroban config identity address user_a) --to $(soroban config identity address rpciege)  --donation_amount 123
+	soroban contract invoke --id $CONTRACT_ID --source user_b --network futurenet -- send_message --from $(soroban config identity address user_b) --to $(soroban config identity address user_a)  --message 48656c6c6f207468657265
+	soroban contract invoke --id $CONTRACT_ID --source user_a --network futurenet -- send_message --from $(soroban config identity address user_a) --to $(soroban config identity address user_b)  --message 48656c6c6f207468657265
+	soroban contract invoke --id $CONTRACT_ID --source user_a --network futurenet -- send_donation --from $(soroban config identity address user_a) --to $(soroban config identity address user_b)  --donation_amount 123
 
-	soroban contract invoke --id $CONTRACT_ID --source rpciege --network futurenet -- get_messages --origin $(soroban config identity address rpciege) --target $(soroban config identity address user_a)   
-	soroban contract invoke --id $CONTRACT_ID --source rpciege --network futurenet -- get_contacts --user_address $(soroban config identity address user_a) 
+	soroban contract invoke --id $CONTRACT_ID --source user_b --network futurenet -- get_messages --origin $(soroban config identity address user_b) --target $(soroban config identity address user_a)   
+	soroban contract invoke --id $CONTRACT_ID --source user_b --network futurenet -- get_contacts --user_address $(soroban config identity address user_a) 
 	
 }
 
@@ -31,6 +31,6 @@ init(){
 	soroban contract invoke --id $CONTRACT_ID --source admin --network futurenet -- initialize --admin $(soroban config identity address admin)  --asset_address $ASSET_ADDRESS	
 }
 
-	soroban contract invoke --id CBGFLK7ENPKPKNW7UQVWZFZHCVZVCY37VXVARVEFZ52V55BPFJ3ZHU7D --source rpciege --network futurenet -- get_messages --origin $(soroban config identity address rpciege) --target GCBELBPTCGSGFT3D2Q53CAQTEA25RDTNOX4I6OCNVMLGYZ4B4TM3VZZH
+	soroban contract invoke --id CBGFLK7ENPKPKNW7UQVWZFZHCVZVCY37VXVARVEFZ52V55BPFJ3ZHU7D --source user_b --network futurenet -- get_messages --origin $(soroban config identity address user_b) --target GCBELBPTCGSGFT3D2Q53CAQTEA25RDTNOX4I6OCNVMLGYZ4B4TM3VZZH
 
  init
